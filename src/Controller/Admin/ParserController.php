3012194dadc3744ee8b1d010b0daf6f0f280e0dc
root <root@4acfc866b767>
2022-04-16 07:03:26 +0000

    display form

--- src/Controller/Admin/ParserController.php
+++ src/Controller/Admin/ParserController.php
@@ -7,6 +7,8 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class ParserController extends AbstractDashboardController
 {
@@ -20,13 +22,20 @@ class ParserController extends AbstractDashboardController
     #[Route('/parser', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
+        $form = $this->createFormBuilder()
+            ->add('URL', TextType::class)
+            ->add('Search', SubmitType::class)
+            ->getForm();
+
+        return $this->render('admin/form.html.twig', [
+            'form' => $form->createView(),
+    ]);
     }
 
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Html');
+            ->setTitle('Parser for Ozon');
     }
 
     public function configureMenuItems(): iterable
