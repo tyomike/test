d6070bad0f716daf62c7a7ca0aeed18aa935460b
Your Name <you@example.com>
2022-04-21 15:54:27 +0300

    edits and need to add updating for all products in DB

---
+++ migrations/Version20220421061720.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+// phpcs:ignoreFile
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Exception;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220421061720 extends AbstractMigration
+{
+    /**
+     * @noinspection PhpMissingParentCallCommonInspection
+     */
+    public function getDescription(): string
+    {
+        return 'TODO: Describe reason for this migration';
+    }
+
+    /**
+     * @noinspection PhpMissingParentCallCommonInspection
+     */
+    public function isTransactional(): bool
+    {
+        return false;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() !== 'mysql',
+            'Migration can only be executed safely on \'mysql\'.'
+        );
+
+        $this->addSql('ALTER TABLE product ADD created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+    }
+
+    /**
+     * @noinspection PhpMissingParentCallCommonInspection
+     *
+     * @throws Exception
+     */
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() !== 'mysql',
+            'Migration can only be executed safely on \'mysql\'.'
+        );
+
+        $this->addSql('ALTER TABLE product DROP created_at');
+    }
+}
--- src/Service/ParserService.php
+++ src/Service/ParserService.php
@@ -33,6 +33,8 @@ class ParserService
         $new = $client->get($url);
         $crawler = new Crawler($new->getBody()->getContents());
 //Crawler
+//        $tag = $crawler->filterXPath("//*[contains(concat(' ', @id, ' '), 'state-searchResultsV2')]")->outerHtml();
+//        $tag = str_replace(['\'></div>', '<div id="state-searchResultsV2', '-252189-default-1" ', '-1304093-default-1" ', 'data-state=\''], '', $tag);
         $tag = $crawler->filterXPath('//*[@id="state-searchResultsV2-252189-default-1"]')->outerHtml();
         $cut = strstr($tag, '{"items');
         $encode = strstr($cut, '\'></div>', true);
@@ -42,7 +44,7 @@ class ParserService
 
         for ($i = 0; $i < $count; $i++) {
             foreach ($encode as $data) {
-//                dd($data);
+//                dd($encode);
                 if (isset($data['multiButton']['ozonSubtitle']['textAtomWithIcon']['text'])) {
 //                dd($test);
                     $price = str_replace([' ₽', ' '], '', $data['mainState'][0]['atom']['price']['price']);
@@ -50,8 +52,8 @@ class ParserService
                     $sku = $data['topRightButtons'][0]['favoriteProductMolecule']['sku'];
                     $review_count = $this->reviewChecker($data['mainState']);
                     $seller = $data['multiButton']['ozonSubtitle']['textAtomWithIcon']['text'];
-                    $seller = strstr($seller, 'продавец');
                     $seller = strip_tags($seller);
+                    $seller = stristr($seller, 'продавец');
                     $seller = str_replace('продавец ', '', $seller);
 
                     $productData = [
@@ -88,51 +90,31 @@ class ParserService
             }
         }
         return 0;
-//        if ($data !== null)
-//            return (int)$data;
-//        else
-//            return 0;
-    }
-
-    public function isSellerAlreadyExists(string $onlyOneSeller)
-    {
-        //Проверка наличия продавца в базе данных под другими id
-        $repository = $this->em->getRepository(Seller::class);
-        $allData = $repository->findAll();
-        foreach ($allData as $sellerData) {
-            if ($onlyOneSeller == $sellerData->getName()) {
-                return $sellerData;
-            }
-        }
-        return 0;
     }
 
     public function saveProduct(array $productData)
     {
         $em = $this->em;
-        $seller = $this->isSellerAlreadyExists($productData['seller']);
-//        if ($seller === 0) {
-        if (empty($seller)) {
-            $seller = new Seller;
-            $seller->setName($productData['seller']);
 
+            $seller = $em->getRepository(Seller::class)->findOneBy(['name' => $productData['seller']]);
+            if (empty($seller)) {
+                $seller = new Seller;
+                $seller->setName($productData['seller']);
+            }
             $em->persist($seller);
             $em->flush();
-        }
 
-        $products = $em->getRepository(Product::class)->findOneBy(['sku' => $productData['sku']]);
-        if (empty($sproducts)) {
-            $products = new Product();
-            $products->setName($productData['name'])
-                ->setPrice($productData['price'])
-                ->setSeller($seller)
-                ->setReviewsCount($productData['review_count'])
-                ->setSku($productData['sku']);
-
-            $em->persist($products);
+            $product = $em->getRepository(Product::class)->findOneBy(['sku' => $productData['sku']]);
+            if (empty($product)) {
+                $product = new Product;
+                $product->setName($productData['name']);
+                $product->setPrice($productData['price']);
+                $product->setSku($productData['sku']);
+                $product->setReviewsCount($productData['review_count']);
+                $product->setSeller($seller);
+            }
+            $em->persist($product);
             $em->flush();
-        }
-
 
         return $productData;
     }
